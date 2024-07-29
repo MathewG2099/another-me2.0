b2f514dc2fc5eaa0b8709f50b8a698ec93f84fd1
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-29 19:10:47 -0400

    Head for me
    -Added aboutm section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,13 @@
 import React from "react";
 import { Container, Row, Col, Card, Image } from "react-bootstrap";
 import avatar from "../images/Imagen de WhatsApp 2024-06-04 a las 12.51.35_5f2e30f5.jpg";
-import "../style/About.css"
+import "../style/About.css";
+
+function About() {
+    return(
+        //This is the About section
+        <section id="aboutm" className="py-f"> 
+
+        </section>
+    )
+}
