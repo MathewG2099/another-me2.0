4504b2d4f837b69203c39321a54c374ee5830bf5
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-29 18:53:01 -0400

    Pic
    -Added the picture on the "about.js" and conect the part for the styles

---
+++ src/components/About.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
+import avatar from "../images/Imagen de WhatsApp 2024-06-04 a las 12.51.35_5f2e30f5.jpg";
+import "../style/About.css"
