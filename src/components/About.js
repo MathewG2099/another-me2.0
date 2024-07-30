6f20fd5eca1935e1f1abe28b7b6c8f1c8f5c8f0a
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-07-29 20:00:04 -0400

    About me info
    -Added a Card section with infomation about me and more things for the page

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,22 @@ function About() {
                    
                 </Col>
                 <Col xs={12} md={9} >
-                
+                <Card>
+                    <Card.Body>
+                        <Card.Title>
+                        Somethig about Mathew Gonzalez 
+                    </Card.Title>
+                    <Card.Text>
+                        Hi everyone ,I'm Mathew Gonzalez, I have 20 years old ,My hopes for the future are to learn more and more about coding and how I can create something that can evolve the way of how we code for the next generation.
+                    </Card.Text>
+                    <Card.Text>
+                        I also start to learning about a.i, I've been making a little project but thats something personal that I want to finish in the future. 
+                    </Card.Text>
+                    <Card.Text>
+                        I like to see this world as a new challenge for my self and make the best not only fo me , for the people that I love and for the people that want to see all my work
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
                 </Col>
             </Row>
         </Container>
